923e10ef87e03845befe9ad9d881510ce22b5223
Lewis Marsden-Lambert <893830+m13t@users.noreply.github.com>
2024-09-13 09:34:37 +0100

    chore: use relative path

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,6 +1,5 @@
 module "example" {
-  source  = "appvia/appvia-audit-role/aws"
-  version = "1.0.0"
+  source  = "../../"
 
   # A secure random string to be used as the role's external ID.
   # This should only be shared between the client and Appvia.
